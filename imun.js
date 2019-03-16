c055bca455f4e299fc6492584e87dc97e51527e4
HyperLoo <43092030+HyperLoo@users.noreply.github.com>
2019-03-16 13:15:30 +0530

    Imun Website

---
+++ imun.js
@@ -0,0 +1,16 @@
+$('ul.nav li.dropdown').hover(function() {
+  $(this).find('.dropdown-menu').stop(true, true).delay(150).fadeIn(300);
+}, function() {
+  $(this).find('.dropdown-menu').stop(true, true).delay(150).fadeOut(300);
+});
+
+$(document).ready(function(){
+			$('.nav-link').click( function(e) {
+				var li = $(this).attr('href');
+				e.preventDefault();
+				
+				$('html,body').animate( {
+					scrollTop:$(li).offset().top
+				},1250);
+			});
+		});
