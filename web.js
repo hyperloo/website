d91cec0edd06711a8b6873923fb20498846238ac
HyperLoo <43092030+HyperLoo@users.noreply.github.com>
2019-03-22 10:55:53 +0530

    update

--- web.js
+++ web.js
@@ -18,9 +18,12 @@ $(document).ready(function(){
 		}, function() {
 			$(this).find('.dropdown-menu').stop(true, true).delay(150).fadeOut(300);
 		});
+		$('a.smooth-scroll').on("click", function(e) {
+            var anchor = $(this);
+            $('html, body').stop().animate({
+                scrollTop: $(anchor.attr('href')).offset().top
+            }, 1000);
+            e.preventDefault();
+		});
+		
 });
-
-
-$(document).ready(function(){
-	
-})
